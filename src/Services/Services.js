47ba771baa6bb3989b04e67fa0f3731831e3eae6
Julia Martelo <julia.martelo@aluno.senai.br>
2025-05-15 16:42:30 -0300

    feat: Subindo o metodo de excluir

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const apiPorta = "5063"
+const apiPorta = "5063";
 
 //apiLocal ela recebe o endereço da api
 const apiLocal = `http://localhost:${apiPorta}/api/`;
-// const apiLocal = `http://localhost:${apiPorta}/api/`;
+// const apiLocal = `http://localhost:5008/api/`;
 
 const api = axios.create({
     baseURL: apiLocal
--- src/pages/cadastroGenero/CadastroGenero.jsx
+++ src/pages/cadastroGenero/CadastroGenero.jsx
@@ -7,6 +7,7 @@ import Footer from "../../components/footer/Footer";
 import Cadastro from "../../components/cadastro/Cadastro";
 import Lista from "../../components/lista/Lista";
 import api from "../../Services/Services";
+import Paginacao from "../../components/paginacao/Paginacao";
 // importar sweet alert
 import Swal from 'sweetalert2';
 
@@ -15,6 +16,8 @@ const CadastroGenero = () => {
 
     //nome do genero
     const [genero, setGenero] = useState("");
+    const [listaGenero, setListaGenero] = useState([]);
+    const [deletaGenero, setDeletaGenero] = useState([]);
 
     function alerta(icone, mensagem) {
         const Toast = Swal.mixin({
@@ -38,26 +41,102 @@ const CadastroGenero = () => {
         evt.preventDefault();
         //Verificar se o input esta vindo vazio
         // trim apaga os espacos vazios
-        if (genero.trim() != "") {
+        if (genero.trim() !== "") {
 
             // try => tentar(o esperado)
             // catch => pega a excecao
             try {
                 // Cadastrar um genero: post
                 await api.post("genero", { nome: genero });
-                alerta("success", "Cadastro realizado com sucesso!")
-             
-                
+                alerta("sucess", "Cadastro realizado com sucesso!")
+
+
             } catch (error) {
-               alerta("error", "Erro! Entre em contato com o suporte!")
-               console.log(error);
-               
+                alerta("error", "Erro! Entre em contato com o suporte!")
+                console.log(error);
+
             }
         } else {
             alerta("Error", "Preencha o campo!")
         }
 
     }
+
+    // função síncrono => Acontece simultaneamente
+    // função assíncrono => Espera uma resposta para acontecer
+    // await => Ele aguarda uma resposta
+    // async => assíncrono
+
+    async function listarGenero() {
+        try {
+            const resposta = await api.get("genero");
+          
+
+            setListaGenero(resposta.data);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // funcao de excluir o genero
+
+    function alertaDelete(icone, mensagem) {
+        Swal.fire({
+            title: "Você tem certeza?",
+            text: "Você não conseguira reverter!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Sim, deletar!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: "Deletado!",
+                    text: "Seu item foi deletado.",
+                    icon: "success"
+                });
+            }
+        });
+    }
+
+
+
+
+
+    async function deletarGenero(generoId) {
+        // console.log("entrou!");
+        try {
+            
+            Swal.fire({
+                title: "Você tem certeza?",
+                text: "Você não conseguira reverter!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Sim, deletar!"
+            }).then(async(result) => {
+                if (result.isConfirmed) {
+                await api.delete(`genero/${generoId.idGenero}`);
+
+                    Swal.fire({
+                        title: "Deletado!",
+                        text: "Seu item foi deletado.",
+                        icon: "success"
+                    });
+                }
+            });
+            listaGenero ();
+            // setDeletaGenero(resposta.data);
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
 
     // Teste
     // useEffect(() => {
@@ -66,6 +145,14 @@ const CadastroGenero = () => {
 
 
     // Fim do teste
+
+    // Assim que a pagina renderizar, o metodo listarGenero() sera chamado
+    useEffect(() => {
+        listarGenero();
+
+    }, [listarGenero])
+
+
 
     return (
         <>
@@ -85,8 +172,16 @@ const CadastroGenero = () => {
                     setValorInput={setGenero}
                 />
                 <Lista tituloDaLista="Lista de Gênero"
-                    excluirNome="none" />
+                    excluirNome="none"
+
+                    // atrubuir para lista, o meu estado atual:
+                    lista={listaGenero}
+
+
+                    funcExcluir = {deletarGenero}
+                />
             </main>
+            <Paginacao/>
             <Footer />
         </>
     )
